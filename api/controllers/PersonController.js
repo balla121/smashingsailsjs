7d05b84df6000c252f862bece88c6945314eea81
Weijie Gao <gweijie01@gmail.com>
2016-02-18 21:01:52 -0500

    added prduction config

--- api/controllers/PersonController.js
+++ api/controllers/PersonController.js
@@ -6,6 +6,12 @@
  */
 
 module.exports = {
-	
+	findOne: function (request, response) {
+		Person.find(request.params.id).exec(function(error, persons){
+			var person = persons[0];
+			person.fullName = person.firstName + ' ' + person.lastName;
+			response.json(person);
+		});
+	}
 };
 
